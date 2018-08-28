9562ad4344077a03cef602732ef57fbc940f0a7a
cirno_poi <874465028@qq.com>
2018-08-28 11:21:12 +0800

    修改tab首项

--- app/src/main/java/com/example/dell/customviewdemo/customview/CustomLayout.java
+++ app/src/main/java/com/example/dell/customviewdemo/customview/CustomLayout.java
@@ -7,6 +7,7 @@ import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 
 import com.example.dell.customviewdemo.R;
@@ -14,7 +15,9 @@ import com.example.dell.customviewdemo.R;
 public class CustomLayout extends FrameLayout {
 
     Button animateBt;
+    Button setNumberBt;
     JikeLikeView jikeLikeView;
+    EditText numberEt;
     private boolean clickFlag = false;
 
     public CustomLayout(Context context) {
@@ -37,8 +40,19 @@ public class CustomLayout extends FrameLayout {
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
 
-        jikeLikeView = (JikeLikeView) findViewById(R.id.jikeLikeView);
-        animateBt = (Button) findViewById(R.id.animate_bt);
+        jikeLikeView = findViewById(R.id.jikeLikeView);
+        animateBt = findViewById(R.id.animate_bt);
+        setNumberBt = findViewById(R.id.set_number_bt);
+        numberEt = findViewById(R.id.set_number_et);
+
+        setNumberBt.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!numberEt.getText().toString().isEmpty()) {
+                    jikeLikeView.setContentInt(Integer.parseInt(numberEt.getText().toString()));
+                }
+            }
+        });
 
         animateBt.setOnClickListener(new OnClickListener() {
             @Override
